4537be1607f2ae46285b3b7a3f0be1e2cd39cd18
vavans <vannnns@gmail.com>
2016-02-17 23:53:01 +0100

    add first servant webservice. Serv a simple select on the sql database

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,14 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE CPP           #-}
+{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeFamilies  #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Main where
 
+--servant imports
+import           Data.Aeson
+import           Data.Time.Calendar
+import           GHC.Generics
+import           Network.Wai
+import           Servant
+import           Network.Wai.Handler.Warp
+--simple sql imports
 import Database.MySQL.Simple
 import Database.MySQL.Simple.QueryResults
 import Database.MySQL.Simple.Result
-import Lib
 
-data Card = Card { idCards :: Int, question :: String, answer :: String, deckId :: Int} deriving Show
+data Card = Card { idCards :: Int, question :: String, answer :: String, deckId :: Int} deriving (Show, Generic)
 
+
+--sql types converter
 instance QueryResults Card where
   convertResults [fic,fq, fa, fdi] [vic,vq, va, vdi] = Card { idCards = ic, question = q, answer = a, deckId = di}
     where ic = convert fic vic
@@ -17,16 +31,34 @@ instance QueryResults Card where
           di = convert fdi vdi
   convertResults fs vs  = convertError fs vs 2
 
-
-
+--sql request select all cards
 select :: IO [Card]
 select = do
   conn <- connect defaultConnectInfo { connectUser = "readapp", connectPassword = "readapp", connectDatabase = "flashCardsdb" }
   query_ conn "select * from Card"
 
 
+--sevant endpoint
+type CardsAPI = "cards" :> Get '[JSON] [Card]
+
+instance ToJSON Card
+
+server1 :: [Card] -> Server CardsAPI
+server1 cards = return cards
+
+cardAPI :: Proxy CardsAPI
+cardAPI = Proxy
+
+app1 :: [Card] -> Application
+app1 cards = serve cardAPI (server1 cards)
+
 main :: IO ()
 main = do
   cards <- select
-  _     <- putStrLn $ show cards
-  return ()
+  run 8081 (app1 cards)
+
+-- main :: IO ()
+-- main = do
+--   cards <- select
+--   _     <- putStrLn $ show cards
+--   return ()
